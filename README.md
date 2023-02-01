9fe7cc3c002fca9394c8ea6217542b08751e85ae
jawahar-lee <116168249+jawahar-lee@users.noreply.github.com>
2023-02-01 16:13:24 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CSK101-M3-L113-Lab-Promises-2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-ztt6yg)
---
+++ promise.js
@@ -0,0 +1,36 @@
+const cookies = [{ name: 'Chocolate Cookies' }, { name: 'Macaron Cookies' }];
+const newCookie = { name: 'Biscotti Cookies' };
+
+// Progression 1: create a function to get all the cookies
+
+// Progression 2: using setTimeout() -- use 1000 units for time parameter
+function getCookies() {
+  setTimeout(() => {
+    let output = '';
+    cookies.forEach((cookie) => {
+      output += `<li>${cookie.name}</li>`;
+    });
+    document.body.innerHTML = output;
+  }, 1000);
+}
+//Progression 3: Create a function to creat cookies and create an object of Promise.
+function pushCookie() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      cookies.push(newCookie);
+      const error = false;
+      if (!error) {
+        resolve();
+      } else {
+        reject('There are no such cookies');
+      }
+    }, 2000);
+  });
+}
+
+// Progression 4: adding two parameters and using setTimeout() -- use 2000 units for time parameter
+
+// Progression 5: handling errors and adding new cookie to the list
+
+pushCookie().then(getCookies);
+// Progression 6: call function using `.then`
